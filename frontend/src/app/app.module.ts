2fca98ff8d267ceb8279708d940c18dbeea17756
futomaki621 <futomaki31plus1@gmail.com>
2020-05-24 16:35:58 +0900

    frontend #2 codemirrorを適用

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -3,14 +3,21 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { GraphViewComponent } from './graph-view/graph-view.component';
+import { FormViewComponent } from './form-view/form-view.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { CodemirrorModule } from '@ctrl/ngx-codemirror';
 
 @NgModule({
   declarations: [
     AppComponent,
-    GraphViewComponent
+    GraphViewComponent,
+    FormViewComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    CodemirrorModule,
+    ReactiveFormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ frontend/src/app/form-view/form-view.component.ts
@@ -0,0 +1,28 @@
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl } from '@angular/forms';
+
+@Component({
+  selector: 'app-form-view',
+  templateUrl: './form-view.component.html',
+  styleUrls: ['./form-view.component.css']
+})
+export class FormViewComponent implements OnInit {
+  profileForm = new FormGroup({
+    firstName: new FormControl(''),
+    lastName: new FormControl(''),
+  });
+  content = null;
+  options = null;
+
+  constructor() { }
+
+  ngOnInit() {
+    // this.profileForm = this.fb.group({})
+    this.options = {
+      lineNumbers :true,
+      theme: 'default',
+      mode: 'markdown',
+    };
+  }
+
+}
